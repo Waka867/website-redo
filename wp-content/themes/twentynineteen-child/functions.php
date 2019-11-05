ddbeeb90d1c4218769609172af2b0b9cbeed661f
Ledwing Hernandez <ledwing.hernandez@gmail.com>
2019-11-05 17:50:01 -0500

    Altered gitignore file so that my twentynineteen child theme will get committed

---
+++ wp-content/themes/twentynineteen-child/functions.php
@@ -0,0 +1,31 @@
+<?php
+
+//require_once(ABSPATH . 'wp-admin/includes/file.php');
+
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+ 
+    $parent_style = 'parent-style'; // This is 'twentynineteen-style' for the Twenty Nineteen theme.
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+
+
+    //wp_enqueue_scripts( 'github-calendar', get_template_directory_uri() . '', '', '', true);
+    //wp_enqueue_scripts( 'github-calendar', $homepath . '/node_modules/github-calendar/dist/github-calendar.min.js', '', '', true);
+ 
+    // This should be changed so that the path to the github-calendar js file allows the file to stay in node_modules and not have to be copied over
+    wp_enqueue_script( 'github-calendar', get_site_url() . '/node_modules/github-calendar/dist/github-calendar.min.js', '', '', true);
+
+
+    //echo get_theme_root();
+    //echo get_site_url();
+}
+
+?>
